50f0fa1217def607d3d35ac683722f3f8c05d3d5
yasin anil <yasinkaya687@gmail.com>
2020-12-30 15:25:06 +0300

    diger gönderim

---
+++ app_server/controllers/digerleri.js
@@ -0,0 +1,12 @@
+
+const hakkinda=function(req, res, next) {
+  res.render('hakkinda', { title: 'Hakkında',
+                         'footer':'Yasin Anıl Kaya Web Teknolojileri ve Programlama 2020',
+                         });
+}
+
+
+module.exports=
+{
+hakkinda    
+}
